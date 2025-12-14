9c94bd8bacb0a40b736a0ea5649ebe6f34d9e1f8
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2025-12-13 22:17:40 -0500

    chore: inline jest config (drop shared base)

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,22 @@
-import baseConfig from '../jest.config.base.mjs';
-
 export default {
-  ...baseConfig,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   roots: ['<rootDir>/test'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true, tsconfig: { module: 'ESNext' } }],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^chalk$': '<rootDir>/__mocks__/chalk.js',
+  },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/index.ts',
+    '!<rootDir>/src/**/*.d.ts',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
   coverageProvider: 'v8',
 };
